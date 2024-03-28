fbf726403d3d07a178aea770cb27e4f5010ae2d8
Aditya Karmakar <adkarmakar521@gmail.com>
2024-03-28 19:29:36 +0530

    added readme,models

---
+++ backend/models/msg.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const msgSchema = new mongoose.Schema(
+  {
+    senderId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    receiverId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true, //automatically add createdAt and updatedAt fields
+  }
+);
+
+const message = mongoose.model("message", msgSchema);
+
+export default message;
