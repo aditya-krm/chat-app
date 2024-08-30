603c5bca2673a27be2d39328dbf168286408a6f9
Aditya Karmakar <adkarmakar521@gmail.com>
2024-08-30 14:25:49 +0530

    production

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
@@ -8,18 +9,26 @@ import userRoutes from "./routes/user.routes.js";
 
 import { connectToMongoDB } from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
-
 dotenv.config();
+
+const PORT = process.env.PORT;
+
+const __dirname = path.resolve();
+
 app.use(express.json());
 app.use(cookieParser());
-
-const PORT = process.env.PORT || 3000;
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/client/dist/index.html"));
+});
+
 server.listen(PORT, () => {
   connectToMongoDB();
-  console.log(`Server is running on http://localhost:${PORT}`);
+  // console.log(`Server is running on http://localhost:${PORT}`);
 });
